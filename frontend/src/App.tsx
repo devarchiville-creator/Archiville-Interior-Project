32792debb8b913cbed69444eae086463432bdc9f
devarchiville-creator <devarchiville@gmail.com>
2026-04-22 14:52:01 +0530

    testing for deployment

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,7 +4,7 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import Landing from "./pages/Landing";
-import Auth from "./pages/Auth";
+// import Auth from "./pages/Auth";
 import Dashboard from "./pages/Dashboard";
 import Project from "./pages/Project";
 import NotFound from "./pages/NotFound.tsx";
@@ -20,7 +20,7 @@ const App = () => (
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Landing />} />
-          <Route path="/auth" element={<Auth />} />
+          {/* <Route path="/auth" element={<Auth />} /> */}
           <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/project/:id" element={<Project />} />
           <Route path="*" element={<NotFound />} />
